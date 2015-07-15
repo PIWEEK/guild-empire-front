4f225390b25efb584b8a3eb79263d5482a63d263
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2015-07-15 16:06:08 +0200

    music player link in div not text

--- src/js/components/Music.js
+++ src/js/components/Music.js
@@ -29,15 +29,18 @@ class Music extends React.Component {
         let musicOn = this.props.musicOn;
 
         return (
-            <div className="guild-music btn-green">
+            <div className="guild-music">
+                <a href="#" onClick={this.musicToggle.bind(this)}>
+                    <div className="btn-green">
+                        <span>Music {musicOn?'ON':'OFF'}</span>
 
-                <a href="#" onClick={this.musicToggle.bind(this)}>Music {musicOn?'ON':'OFF'}</a>
-
-                <audio ref="backgroundMusic" autoPlay loop>
-                    <source src="./music/guild-music.mp3"/>
-                    <source src="./music/guild-music-2.mp3"/>
-                    Your browser does not support the audio element.
-                </audio>
+                        <audio ref="backgroundMusic" autoPlay loop>
+                            <source src="./music/guild-music.mp3"/>
+                            <source src="./music/guild-music-2.mp3"/>
+                                Your browser does not support the audio element.
+                        </audio>
+                    </div>
+                </a>
             </div>
         );
     }
